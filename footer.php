e38c4a4c567b106293ab91ba640f0932d955f671
jgv <jvingiano@gmail.com>
2010-12-30 12:46:34 -0500

    moved js to footer and getting description from bloginfo()

--- footer.php
+++ footer.php
@@ -9,5 +9,11 @@
 		<p>Theme created by <a href="http://itsallopticks.com">Opticks, Inc.</a> for <a href="http://www.colt-rane.com">Colt + Rane.</a></p>
 	</div>
 
+	<script src="<?php bloginfo('template_url') ?>/js/jquery-1.4.2.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="<?php bloginfo('template_url') ?>/js/jquery.lazyload.mini.js" type="text/javascript" charset="utf-8"></script>
+	<script src="<?php bloginfo('template_url') ?>/js/infinite-scroll.js" type="text/javascript" charset="utf-8"></script>
+	<script src="<?php bloginfo('template_url') ?>/js/jquery.tipTip.minified.js" type="text/javascript" charset="utf-8"></script>
+	<script src="<?php bloginfo('template_url') ?>/js/functions.js" type="text/javascript" charset="utf-8"></script>
+
 </body>
 </html>
--- header.php
+++ header.php
@@ -10,17 +10,15 @@
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" />
-	<meta name="description" content="Colt + Rane is an art, design and culture blog." />
+	<meta name="description" content="<?php bloginfo('description')?>" />
 	
 	<title><?php if (function_exists('is_tag') && is_tag()) { echo 'Posts tagged &quot;'.$tag.'&quot; - '; } elseif (is_archive()) { wp_title(''); echo ' Archive - '; } elseif (is_search()) { echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; } elseif (!(is_404()) && (is_single()) || (is_page())) { wp_title(''); echo ' - '; } elseif (is_404()) { echo 'Not Found - '; } bloginfo('name'); ?></title>
 	<link rel="shortcut icon" href="http://colt-rane.com/favicon.ico" type="image/x-icon" />
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 
-	<script src="<?php bloginfo('template_url') ?>/js/jquery-1.4.2.min.js" type="text/javascript" charset="utf-8"></script>
-	<script src="<?php bloginfo('template_url') ?>/js/jquery.lazyload.mini.js" type="text/javascript" charset="utf-8"></script>
-	<script src="<?php bloginfo('template_url') ?>/js/infinite-scroll.js" type="text/javascript" charset="utf-8"></script>
-	<script src="<?php bloginfo('template_url') ?>/js/jquery.tipTip.minified.js" type="text/javascript" charset="utf-8"></script>
-	<script src="<?php bloginfo('template_url') ?>/js/functions.js" type="text/javascript" charset="utf-8"></script>
+  <!--[if lt IE 9]>
+      <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+  <![endif]--> 
 	
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 	<link rel="stylesheet" href="<?php bloginfo('template_url') ?>/tipTip.css" type="text/css" media="screen" />
