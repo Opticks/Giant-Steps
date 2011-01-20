bf5ea752c6135350c4d1ccdab0c52a7039b61881
jgv <jvingiano@gmail.com>
2011-01-20 11:14:36 -0500

    adding meta properties, converting to html5

--- header.php
+++ header.php
@@ -4,13 +4,20 @@
  *	@subpackage Giant Steps
  */
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<!--[if IE]><![endif]--> 
+<html lang="en-us" dir="ltr">
 <head profile="http://gmpg.org/xfn/11">
-	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta http-equiv="imagetoolbar" content="false" /> 
+  <meta name="application-name" content="<?php bloginfo('name')?>" />
+  <meta name="msapplication-tooltip" content="<?php bloginfo('description')?>" />  
+  <meta name="msapplication-starturl" content="<?php bloginfo('url')?>/#pinned" />
 	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" />
 	<meta name="description" content="<?php bloginfo('description')?>" />
+  <meta property="og:site_name" content="<?php bloginfo('name')?>" /> 
+  <link rel="canonical" href="<?php bloginfo('url')?>" />
 	
 	<title><?php if (function_exists('is_tag') && is_tag()) { echo 'Posts tagged &quot;'.$tag.'&quot; - '; } elseif (is_archive()) { wp_title(''); echo ' Archive - '; } elseif (is_search()) { echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; } elseif (!(is_404()) && (is_single()) || (is_page())) { wp_title(''); echo ' - '; } elseif (is_404()) { echo 'Not Found - '; } bloginfo('name'); ?></title>
 	<link rel="shortcut icon" href="<?php bloginfo('template_url')?>/images/favicon.ico" type="image/x-icon" />
