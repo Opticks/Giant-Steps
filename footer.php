d7d23c6b6efe30a97266b24247fefd954070d900
jgv <jvingiano@gmail.com>
2011-04-27 12:07:52 -0400

    fixing script tag and upgrading to jquery 1.5.2

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
 		<p>Theme created by <a href="http://itsallopticks.com">Opticks, Inc.</a> for <a href="http://www.colt-rane.com">Colt + Rane.</a></p>
 	</footer>
 
-    <%= javascript_include_tag "https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js" %>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
     <script>!window.jQuery && document.write(unescape('%3Cscript src="<?php bloginfo('template_url') ?>/js/jquery-1.4.2.min.js"%3E%3C/script%3E'))</script>
 
 	<script src="<?php bloginfo('template_url') ?>/js/jquery.lazyload.mini.js"></script>
